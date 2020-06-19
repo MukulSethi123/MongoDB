296b2f1a80da236c5ddb53467438e8a4667d4c69
Mukul <mukul.ajay.1499@gmail.com>
2020-06-19 23:30:27 +0530

    commit 2.0.0: completed all functionality of the website completed the Boss Level challenge 3;

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+const { kebabCase } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,9 +18,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts = [{}];
+const posts = [];
 app.get("/", function(req, res) {
-    res.render("home", { homeContent: homeStartingContent, posts: posts });
+    res.render("home", { homeContent: homeStartingContent, posts: posts, lodash: _ });
 });
 
 app.get("/about", function(req, res) {
@@ -34,16 +36,12 @@ app.get("/compose", function(req, res) {
 });
 
 app.get("/posts/:postName", function(req, res) {
-    let postsName = req.params.postName;
+    let postsName = _.lowerCase(req.params.postName);
     posts.forEach(function(post) {
-        let postTitle = post.title;
-        if (postsName === postTitle.toLowerCase()) {
-            console.log("match found");
+        if (postsName === _.lowerCase(post.title)) {
+            res.render("post", { post: post });
         }
     });
-
-    // let postTitle = req.params.postName;
-    // if(postTitle === posts[1].post.title)
 })
 app.post("/compose", function(req, res) {
     const post = {
@@ -53,14 +51,6 @@ app.post("/compose", function(req, res) {
     posts.push(post);
     res.redirect("/");
 });
-
-
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
